c5be7bfa5d223b930b6fdd71e4273e0f88620f5e
USER <prudkovitalik@gmail.com>
2024-12-18 21:26:34 +0200

    18/11

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_cubit/widgets/app_large_text.dart';
+import 'package:travel_app_cubit/widgets/app_text.dart';
+
+import '../utils/color.dart';
+import '../widgets/responsive_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  List<String> images = [
+    "images/welcome_page_1.jpg",
+    "images/welcome_page_2.jpg",
+    "images/welcome_page_3.jpg",
+  ];
+
+  List<String> texts = ["Discover", "Discover", "Discover"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  images[index],
+                ),
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(text: "Mountain", size: 30),
+                      const SizedBox(height: 20),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredible sense of freedom along with endurance test",
+                          size: 16,
+                          color: AppColors.textColor2,
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      ResponsiveButton(
+                        width: 120,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: List.generate(
+                      3,
+                      (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 6),
+                          width: 8,
+                          height: index == indexDots ? 25 : 8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index == indexDots
+                                ? AppColors.mainColor
+                                : AppColors.mainColor.withOpacity(0.3),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
